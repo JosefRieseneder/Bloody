e73fc8a3cec153bdcff6532811040d1f766ce236
Rithe <Rithe@10.28.1.52>
2013-11-28 09:09:02 +0100

    charts demo + lib

---
+++ SourceCode/src/at/fhooe/mhs/bloody/charts/ChartsActivity.java
@@ -0,0 +1,8 @@
+package at.fhooe.mhs.bloody.charts;
+
+import android.app.Activity;
+
+public class ChartsActivity extends Activity
+{
+
+}
--- SourceCode/src/at/fhooe/mhs/bloody/diagram/DiagramActivity.java
+++
@@ -1,8 +0,0 @@
-package at.fhooe.mhs.bloody.diagram;
-
-import android.app.Activity;
-
-public class DiagramActivity extends Activity
-{
-
-}
